ae0fc3a708d445fa694331ec2a7f3e68d5b6ab84
Ian Eisenberg <ian.eisenberg@gmail.com>
2018-06-25 14:36:26 +0200

    try xvfb-run with --

--- karma.conf.js
+++ karma.conf.js
@@ -30,7 +30,7 @@ module.exports = function (config) {
     };
 
     if(process.env.TRAVIS) {
-        configuration.browsers = ['ChromeHeadless'];
+//        configuration.browsers = ['ChromeHeadless'];
     }
 
     config.set(configuration);
