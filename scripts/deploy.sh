94c4f9920b72c6e70891c96fcb843fb1da5f94bc
Ian Eisenberg <ian.eisenberg@gmail.com>
2018-06-28 11:33:25 +0200

    rsync to update files on server

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-
-ssh "${HOSTING_HOST}" -p "${HOSTING_PORT}" -i vimexx_travis_openssh -l "${HOSTING_USERNAME}" touch /home/${HOSTING_USERNAME}/hello
+rsync -zva --delete -e "ssh ${HOSTING_HOST} -p ${HOSTING_PORT} -i vimexx_travis_openssh -l ${HOSTING_USERNAME}" dist/ ${HOSTING_HOST}:/home/${HOSTING_USERNAME}/domains/eisenguy.nl/public_html/
