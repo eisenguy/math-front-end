10f9322ab08383ca0a48a448412303fe06e24d61
Ian Eisenberg <ian.eisenberg@gmail.com>
2018-06-28 12:49:25 +0200

    simplify and correct rsync command

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-rsync -zva --delete -e "ssh ${HOSTING_HOST} -p ${HOSTING_PORT} -i vimexx_travis_openssh -l ${HOSTING_USERNAME}" dist/ ${HOSTING_HOST}:/home/${HOSTING_USERNAME}/domains/eisenguy.nl/public_html/
+rsync -e "ssh -p $HOSTING_PORT -i vimexx_travis_openssh" -zva --delete dist/ ${HOSTING_USERNAME}@${HOSTING_HOST}:/home/${HOSTING_USERNAME}/domains/eisenguy.nl/public_html/
