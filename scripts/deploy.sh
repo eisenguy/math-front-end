6c1c16992a7fd24933397302e221aa3ef42d0de1
Ian Eisenberg <ian.eisenberg@gmail.com>
2018-06-28 10:04:10 +0200

    actually modify something on the server

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,4 +4,4 @@ echo "I have made it into the deploy script :)"
 pwd
 ls -l
 
-ssh web0101.zxcs.nl -p 7685 -i vimexx_travis_openssh -l u31699p26638 whoami
+ssh web0101.zxcs.nl -p 7685 -i vimexx_travis_openssh -l u31699p26638 touch /home/u31699p26638/hello
